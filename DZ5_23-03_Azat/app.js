6ac3ef743ac8efd18a222ec4d2b5686e5084d93c
azat <zacsun03@gmail.com>
2023-03-28 22:22:11 +0600

    DZ5

---
+++ DZ5_23-03_Azat/app.js
@@ -0,0 +1,34 @@
+const usdInput = document.querySelector('#usd'),
+    kgsInput = document.querySelector('#kgs'),
+    eurInput = document.querySelector('#eur')
+
+const convert = (elem, target1, target2) => {
+
+    elem.oninput = () => {
+
+        const req = new XMLHttpRequest()
+        req.open('GET', 'data.json')
+        req.setRequestHeader('Content-type', 'application/json')
+        setTimeout(() => req.send(), 0)
+
+        req.onload = () => {
+
+            const res = JSON.parse(req.responseText)
+            if (elem.value === '') {
+                target1.value = 0
+                target2.value = 0
+            } else {
+                //  res[ 'elem.id - target1.id' ] -> res['usd-kgs']
+                target1.value = elem.value * res[`${elem.id}-${target1.id}`]
+                target2.value = target1.value * res[`${target1.id}-${target2.id}`]
+            }
+
+        }
+    }
+}
+
+convert(usdInput, kgsInput, eurInput)
+convert(eurInput, usdInput, kgsInput)
+convert(kgsInput, eurInput, usdInput)
+
+
